1f16ff681b3a35b7d87943b6e27c6e9039c42c8c
DruboSaptapan <saptapan2017@gmail.com>
2022-01-01 19:10:01 +0600

    banner-updated

--- src/Pages/Request/Request.js
+++ src/Pages/Request/Request.js
@@ -10,10 +10,10 @@ const Request = () => {
                 <Row className="w-75 mx-auto">
                     <div className="col-8">
                         {/* <h3 className="repair my-4">আপনার কি নতুন  কিছু  তৈরি  বা মেরামত করা দরকার?</h3> <br /> */}
-                        <h3 className="repair my-4">Do you want to request for any service?</h3>
+                        <h3 className="repair my-4 text-end">Do you want to request for any service?</h3>
                     </div>
-                    <div className="col-4 my-auto">
-                        <Link to="/repair" className="read-more apply btn btn-success">Click here</Link>
+                    <div className="col-4 my-auto ps-0">
+                        <Link to="/repair" className="read-more apply btn btn-success rounded-pill px-3">Click here</Link>
                     </div>
                 </Row>
             </Container>
